fb2c43612fe26e659afdd3a7e3f974041ee1656a
tatendatembojnr-code <tatendatembojnr@gmail.com>
2026-06-01 09:47:52 +0200

    Fix installation error: remove deprecated tax_inclusive setting from post_init_hook

--- hooks.py
+++ hooks.py
@@ -5,13 +5,5 @@ _logger = logging.getLogger(__name__)
 
 
 def post_init_hook(env):
-    """Post-installation hook to set tax inclusive by default"""
-    
-    # Set tax inclusive for all companies
-    companies = env['res.company'].search([])
-    for company in companies:
-        if hasattr(company, 'tax_calculation_rounding_method') and company.tax_calculation_rounding_method != 'tax_inclusive':
-            company.tax_calculation_rounding_method = 'tax_inclusive'
-            _logger.info("Set tax inclusive for company: %s", company.name)
-    
-    _logger.info("Post-install hook completed: Tax Inclusive set for all companies")
+    """Post-installation hook"""
+    _logger.info("Post-install hook completed")
